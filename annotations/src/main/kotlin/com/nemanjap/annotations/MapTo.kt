441690a44dc24245e11f0df78669d35055ac5054
Nemanja Pluzarev <nemanja.pluzarev@target-video.com>
2025-08-13 09:06:38 +0200

    Added documentation on every class

--- annotations/src/main/kotlin/com/nemanjap/annotations/MapTo.kt
+++ annotations/src/main/kotlin/com/nemanjap/annotations/MapTo.kt
@@ -2,6 +2,63 @@ package com.nemanjap.annotations
 
 import kotlin.reflect.KClass
 
+/**
+ * Declares that the annotated class should have a generated mapper to the given target type.
+ *
+ * This annotation is typically processed by a code generator (e.g., KSP) to create
+ * mapping functions between the annotated class (source) and the specified `target` class.
+ * It also allows customizing the mapping behavior through additional parameters.
+ *
+ * Example:
+ * ```
+ * @MapTo(
+ *     target = UserDomain::class,
+ *     suspendable = true,
+ *     propertyMaps = [
+ *         PropertyMap(from = "firstName", to = "givenName")
+ *     ]
+ * )
+ * data class UserDto(val firstName: String, val age: Int)
+ * ```
+ *
+ * The generator could then produce:
+ * ```
+ * suspend fun UserDto.toUserDomain(): UserDomain { ... }
+ * ```
+ *
+ * @property target
+ * The target class to which this source class should be mapped.
+ *
+ * @property suspendable
+ * If `true`, the generated mapper function will be marked as `suspend`.
+ *
+ * @property oneLineEnabled
+ * If `true`, the mapping will be generated in a compact single-line style
+ * (useful for short mappings without intermediate variables).
+ *
+ * @property sourceNullable
+ * If `true`, the source type will be nullable in the generated function
+ * signature (e.g., `fun MyDto?.toDomain(): MyDomain`).
+ *
+ * @property targetNullable
+ * If `true`, the target type will be nullable in the generated function
+ * return type (e.g., `fun MyDto.toDomain(): MyDomain?`).
+ *
+ * @property isSingleton
+ * If `true`, the generated mapper will be registered as a singleton in the DI container
+ * (e.g., using `single` in Koin). If `false`, it will be a factory.
+ *
+ * @property generateReverse
+ * If `true`, the generator will also create a reverse mapping function from `target` to source.
+ *
+ * @property generateExtensions
+ * If `true`, the generator will create extension functions for mapping
+ * instead of (or in addition to) standalone mapper classes.
+ *
+ * @property propertyMaps
+ * An array of `PropertyMap` entries that define custom name mappings
+ * between source and target properties when their names differ.
+ */
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.SOURCE)
 annotation class MapTo(
--- annotations/src/main/kotlin/com/nemanjap/annotations/mapper/SuspendMapper.kt
+++ annotations/src/main/kotlin/com/nemanjap/annotations/mapper/SuspendMapper.kt
@@ -1,5 +1,41 @@
 package com.nemanjap.annotations.mapper
 
+/**
+ * A generic functional interface for mapping an object of type [Input] to an object of type [Output] in a suspending context.
+ *
+ * This interface is intended for mapping operations that require coroutine support,
+ * such as performing asynchronous or non-blocking transformations.
+ *
+ * It is commonly used in service or repository layers where mapping might involve
+ * database queries, network calls, or other suspend functions.
+ *
+ * Example usage:
+ * ```
+ * class UserDtoToDomainSuspendMapper : SuspendMapper<UserDto, UserDomain> {
+ *     override suspend fun mappingObject(input: UserDto): UserDomain {
+ *         // Example: asynchronous transformation
+ *         val extraInfo = api.fetchExtraInfo(input.id)
+ *         return UserDomain(id = input.id, name = input.name, extra = extraInfo)
+ *     }
+ * }
+ * ```
+ *
+ * Using a lambda:
+ * ```
+ * val mapper = SuspendMapper<UserDto, UserDomain> { dto ->
+ *     UserDomain(id = dto.id, name = dto.name)
+ * }
+ * ```
+ *
+ * @param Input  The source type to map from.
+ * @param Output The target type to map to.
+ */
 fun interface SuspendMapper<in Input, out Output> {
+    /**
+     * Maps the given [input] object to an instance of [Output] in a suspending context.
+     *
+     * @param input The source object to be transformed.
+     * @return The mapped target object.
+     */
     suspend fun mappingObject(input: Input): Output
 }
